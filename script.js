91b62a412fa881e474f9f306c9377841becf86e8
Rodrigo.Luciano <brdc.cxlviii1@huawei.com>
2024-01-11 16:33:33 -0300

    dd sound and counter to buttton iniciar

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const startPauseBt = document.querySelector('#start-pause')
+
 musica.loop = true;
+musica.volume = 0.1;
+
+let tempoDecorridoEmSegundos = 5;
+
+let intervaloId = null;
+
 
 musicaFocoInput.addEventListener('change', () => {
     if(musica.paused){
@@ -62,4 +70,29 @@ function alteraContexto(contexto){
             break;
 
     }
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0){
+    zerar()
+        alert('Tempo Finalizado!')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1;
+    console.log('Temporizador:' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click',iniciarOuPausar)
+
+function iniciarOuPausar(){
+if(intervaloId){
+    zerar()
+    return
+}
+    intervaloId = setInterval(contagemRegressiva,1000)
+}
+
+function zerar(){
+    clearInterval(intervaloId)
+    intervaloId = null
 }
