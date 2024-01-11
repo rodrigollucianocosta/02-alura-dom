ff8e7d9aa0022cb44aa375869fad39dfd6727458
Rodrigo.Luciano <brdc.cxlviii1@huawei.com>
2024-01-11 11:20:25 -0300

    criando função alteraContexto

--- script.js
+++ script.js
@@ -5,16 +5,24 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner =document.querySelector('.app__image')
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src','/imagens/foco.png')
+    // html.setAttribute('data-contexto', 'foco')
+    // banner.setAttribute('src','/imagens/foco.png')
+    alteraContexto('foco');
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src','/imagens/descanso-curto.png')
+    // html.setAttribute('data-contexto', 'descanso-curto')
+    // banner.setAttribute('src','/imagens/descanso-curto.png')
+    alteraContexto('descanso-curto');
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src','/imagens/descanso-longo.png')
+    //html.setAttribute('data-contexto', 'descanso-longo')
+    //banner.setAttribute('src','/imagens/descanso-longo.png')
+    alteraContexto('descanso-longo');
 })
+
+function alteraContexto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
